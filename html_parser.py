ab9e328676f8f4c5b12f6ce9f2870ebc997b743a
pythonsemicolon <taz@tazs-Mac-mini.local>
2023-08-08 09:37:52 -0400

    Adapt Zotero script fixes from other static websites Code changes imported from SIMSSA static website modifications.

--- html_parser.py
+++ html_parser.py
@@ -1,11 +1,8 @@
 from bs4 import BeautifulSoup
 from urllib.parse import unquote
-import re
-import os
-import markdown
-import shutil
+import json
 
-print('Media (m,M), presentations (pr, PR), publications (pu, PU), or all (a,A)?\n')
+print('Media (m,M), presentations (pr, PR), publications (pu, PU) or all (a,A)?\n')
 choice = str(input()).lower()
 
 input_list = ['m', 'pr', 'pu', 'a']
@@ -20,65 +17,57 @@ if choice == 'a':
 else:
     parse_list = [full_list[input_list.index(choice)]]
 
-mirai_root_folder = './'
+ddmal_root_folder = './'
 export_folder = 'zotero_export/'
-
-# if os.path.exists():
-#     shutil.rmtree(mirai_root_folder + citation_folder + '/' + year)
 
 for type in parse_list:
 
     html_file_name = f'MIRAI_{type}.html'
-    citation_folder = f'_{type}'
+    path = f'activities/{type}/content.json'
 
-    with open(export_folder + html_file_name) as f:
+    # Dictionaries for each of the different sources. Keys are the years, values are the html contents.
+    # These will be stored in JSON files in the corresponding folders.
+    content = {}
+
+    with open(export_folder + html_file_name, encoding='utf-8') as f:
         html_soup = BeautifulSoup(f, 'html.parser')
 
-    shutil.rmtree(citation_folder)
-    os.makedirs(citation_folder)
-    # Save html (div) and ascii title [ [<div></div>, "Example Title"]]
 
     html_array = []
 
     for html_tag in html_soup.findAll('div', {'class': 'csl-entry'}):
-        # print(tag.find_next('span'))
-        # if html_tag.find('a'):
-        #     continue
         parse_attr = html_tag.find_next('span')['title']
         year = 'n.d.'
         author = 'no_author'
         title = ')no_title'
         a_title = ')no_a_title'
         b_title = ')no_b_title'
+
         if 'rft.date' in parse_attr:
             year = parse_attr.split('rft.date=')[1].split('-')[0].split('&')[0]
-        if 'rft.aulast' in parse_attr:
-            author = unquote(parse_attr.split('rft.aulast=')[1].split('&')[0])
-        if 'rft.title' in parse_attr:
-            title = unquote(parse_attr.split('rft.title=')[1].split('&')[0])
-        if 'rft.atitle' in parse_attr:
-            a_title = unquote(parse_attr.split('rft.atitle=')[1].split('&')[0])
-        if 'rft.btitle' in parse_attr:
-            b_title = unquote(parse_attr.split('rft.btitle=')[1].split('&')[0])
 
-        final_title = ''
-        for t in [title, a_title, b_title]:
-            if t.split('_')[0] != ')no':
-                final_title = t
-                break
-        file_name = author + '_' + final_title.replace(' ', '_') + '_' + year + '.md'
+        # might need later
+        # if 'rft.aulast' in parse_attr:
+        #     author = unquote(parse_attr.split('rft.aulast=')[1].split('&')[0])
+        # if 'rft.title' in parse_attr:
+        #     title = unquote(parse_attr.split('rft.title=')[1].split('&')[0])
+        # if 'rft.atitle' in parse_attr:
+        #     a_title = unquote(parse_attr.split('rft.atitle=')[1].split('&')[0])
+        # if 'rft.btitle' in parse_attr:
+        #     b_title = unquote(parse_attr.split('rft.btitle=')[1].split('&')[0])
 
-        if not os.path.exists(mirai_root_folder + citation_folder + '/' + year):
-            os.makedirs(mirai_root_folder + citation_folder + '/' + year)
-        with open(mirai_root_folder + citation_folder + '/' + year + '/' + file_name, 'w') as f:
-            f.write(f'---\npresentation_year: {year}\nyear: {year}\n---\n\n{html_tag.decode_contents()}')
 
-        print(html_tag.decode_contents(), '\n')
-        print(parse_attr, '\n')
-        print('T', final_title, '\n\n')
+        if year in content:
+            content[year].append(html_tag.decode_contents())
+        else:
+            content[year] = [html_tag.decode_contents()]
+    
+    # sort by year, descending
+    content = {y: content[y] for y in sorted(content, reverse=True)}
 
-    # print("unsorted")
-    # for x in html_array: print(x[0], x[1])
-    html_array = sorted(html_array, key = lambda x: (x[0], x[1]))
-    # print("\nsorted")
-    # for x in html_array: print(x[0], x[1], x[2], "\n")
+    # sort alphabetically in each year
+    for y in content:
+        content[y].sort()
+
+    with open(path, 'w') as f:
+        json.dump(content, f, indent=4)
